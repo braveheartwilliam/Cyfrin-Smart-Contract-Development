875f1dc246d4da24cb628848ba9298f11e1de492
braveheartwilliam <braveheartwilliam@protonmail.com>
2025-01-06 09:01:08 -0500

    final FundMe

--- FundMe/FundMe_V2_ETH.sol
+++ FundMe/FundMe_V2_ETH.sol
@@ -16,8 +16,10 @@ contract FundMe {
 
     //uint256 public minimumUSD = 5 * 1e18; // or 5e18
 
-    uint256 public constant MINIMUM_USD = 5 * 1e18; // SAVES GAS
-
+    uint256 public constant MINIMUM_USD = 5; // IN $$$$    SAVES GAS;
+    uint256 internal minimumWEI = 0; // minimum in dollars converted to WEI using the PriceConverter ($  ->WEI)
+    uint256 public iValue = 999;
+    uint256 public valueWEI = 0; // message value already in WEI
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
     address public immutable i_owner;
@@ -25,32 +27,28 @@ contract FundMe {
     constructor() {
         i_owner = msg.sender; // original establishment of contract on blockchain by creator that deployed the contract
     }
+    function  getMimimumWEI() public view returns(uint256) {
+        return minimumWEI;
+    }
 
     function fund() public payable {
         //msg.value.getConversionRate() -- uses msg.value as the input to the function getConversionRate
+        //minimumWEI = MINIMUM_USD.getConversionRate();
+        minimumWEI = PriceConverter.getConversionRate(MINIMUM_USD);
+
+        iValue = msg.value;
 
         // require(getConversionRate(msg.value) >= minimumUSD, "didn't sent enough Wei"); // 1e18 = 1 ETH = 1000000000000000000 wei = 1 * 10**18
-        require(
-            msg.value.getConversionRate() >= MINIMUM_USD,
-            "didn't sent enough Wei"
-        ); // 1e18 = 1 ETH = 1000000000000000000 wei = 1 * 10**18
+        //require(valueWEI >= minimumWEI, "didn't send enough Wei"); // 1e18 = 1 ETH = 1000000000000000000 wei = 1 * 10**18
         funders.push(msg.sender);
         //addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
         addressToAmountFunded[msg.sender] += msg.value;
+        //minimumWEI = 0;
     }
 
     function withdraw() public onlyOwner {
         // for loop
         //require(msg.sender == owner); --- instead use a Solidity "modifier" ( see below)
-        for (
-            /* starting index, ending index, step amount*/
-            uint256 funderIndex = 0;
-            funderIndex < funders.length;
-            funderIndex++
-        ) {
-            address funder = funders[funderIndex];
-            addressToAmountFunded[funder] = 0;
-        }
 
         funders = new address[](0);
 
@@ -58,7 +56,7 @@ contract FundMe {
         // //1. transfer - AUTOMATICALLY REVERTS IF TRANSFER FAILS
         // //msg.sender is type address
         // // payable(mesg.sender) is type payable
-        // payable(msg.sender).transfer(address(this).balance);
+        payable(msg.sender).transfer(address(this).balance);
 
         // //2. Send - DOES NOT AUTOMATICALLY REVER IF SEND FAILS; MUST CHECK FOR FAILURE AND REVERT IN CODE
         //bool sendSuccess = payable(msg.sender).send(address(this).balance);
@@ -68,17 +66,17 @@ contract FundMe {
 
         //(bool callSuccess, bytes memory dataReturned) = payable(msg.sender).call{value: address(this).balance}("");
         // Don't need dataReturned so can ignore by not referencing.
-        (bool callSuccess, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(callSuccess, "call failed");
+        //(bool callSuccess, ) = payable(msg.sender).call{
+        //    value: address(this).balance
+        // }("");
+        // require(callSuccess, "call failed");
     }
 
     modifier onlyOwner() {
         //require(msg.sender == i_owner, "Sender is not owner"); // message sender must be owner
 
         //The following replaces above and uses less gas.  Note: NotOwner defined outside of contract with type "error"
-        if (msg.sender == i_owner) {
+        if (msg.sender != i_owner) {
             revert NotOwner();
         }
 
@@ -88,6 +86,7 @@ contract FundMe {
     receive() external payable {
         fund();
     }
+
     fallback() external payable {
         fund();
     }
--- FundMe/PriceConverter.sol
+++ FundMe/PriceConverter.sol
@@ -4,28 +4,35 @@ pragma solidity ^0.8.24;
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 library PriceConverter {
+
+    
     function getPrice() internal view returns (uint256) {
         // address: 0x694AA1769357215DE4FAC081bf1f309aDC325306
         // ABI
 
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
-            0x694AA1769357215DE4FAC081bf1f309aDC325306
+           0x694AA1769357215DE4FAC081bf1f309aDC325306
         );  // address for Sepolia testnet
         (, int256 price, , , ) = priceFeed.latestRoundData();
         // price of ETH in terms of USD
         // doesn't include a decimal so 200000000000 is 2000.00000000
-        return uint256(price * 1e10);
+        return uint256(price);
+        //return uint256(price);
     }
 
-    function getConversionRate(uint256 ethAmount)
+    function getConversionRate(uint256 minUSD)  //ethA
         internal
         view
         returns (uint256)
     {
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethAmount * ethPrice) / 1e18;
-        // 1000000000000000000 * 1000000000000000000 = 1000000000000000000000000000000000000 so need to divide out 18 zeroes
-        return ethAmountInUsd;
+        uint256 minWEI = 888;
+        uint256 ethPrice$ = getPrice() / 1e8;
+        uint256 dummy = ethPrice$ + minUSD;
+        
+        dummy=0;
+        //uint256 minWEI = (minUSD * 1e18) / ethPrice$;    // mult by 1e18 to get into WEI
+        // 5 /(ethPrice/1e10) = minUSD * 1e10 /ethPrice;  --> 5 / 3600 * 1e18
+        return minWEI;
     }
 
     function getVersion() internal view returns (uint256) {
